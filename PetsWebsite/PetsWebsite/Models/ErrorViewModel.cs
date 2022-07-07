17548e65615e5c5315dd2094c4665dd2725fcc52
DESKTOP-J210O8V\TibeMe_user <rc40214@gmail.com>
2022-07-07 09:49:16 +0800

    0707

---
+++ PetsWebsite/PetsWebsite/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace PetsWebsite.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
