cef6e52320f1514da9260fea851dbca1f884aca5
AndrewWu <andrewwu0@gmail.com>
2022-07-15 20:35:50 +0800

    新增會員資訊R / U 功能 & 修改Layout設計

---
+++ PetsWebsite/PetsWebsite/Controllers/PetsController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace PetsWebsite.Controllers
+{
+    public class PetsController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
--- PetsWebsite/PetsWebsite/Controllers/UserDetailsAPIController/UserDetailsApiController.cs
+++ PetsWebsite/PetsWebsite/Controllers/UserDetailsAPIController/UserDetailsApiController.cs
@@ -3,6 +3,7 @@ using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using PetsWebsite.Models;
 using PetsWebsite.Models.ViewModel;
+using System.Collections.Generic;
 
 namespace PetsWebsite.Controllers.UserDetailsAPIController
 {
@@ -12,33 +13,33 @@ namespace PetsWebsite.Controllers.UserDetailsAPIController
     public class UserDetailsApiController : ControllerBase
     {
 
-        UserDetailsViewModel data = new UserDetailsViewModel
-        {
-            LastName = "Alex",
-            FirstName = "Wu",
-            Email = "andfwf@gmail.com",
-            Phone = "22222222",
-            Address = "XXXXXXXX",
-            Birthday = "1995/06/11"
-        };
+        //UserDetailsViewModel data = new UserDetailsViewModel
+        //{
+        //    LastName = "Alex",
+        //    FirstName = "Wu",
+        //    Email = "andfwf@gmail.com",
+        //    Phone = "22222222",
+        //    Address = "XXXXXXXX",
+        //    Birthday = "1995/06/11"
+        //};
 
-        List<UserDetailsViewModel> datalist = new List<UserDetailsViewModel>();
+        //List<UserDetailsViewModel> datalist = new List<UserDetailsViewModel>();
 
         private readonly PetsDBContext _dbContext;
         public UserDetailsApiController(PetsDBContext dbContext)
         {
             _dbContext = dbContext;
-            datalist.Add(data);
+            //datalist.Add(data);
         }
         [HttpGet]
         public async Task<ActionResult<UserDetailsViewModel>> GetUserDetails()
         {
-            int id = 1; 
+            int id = 4; 
             if (_dbContext.Users == null)
             {
                 return NotFound();
             }
-            return _dbContext.Users.Where(u => u.UserId == id).ToList().Select(u => new UserDetailsViewModel()
+            return _dbContext.Users.Where(u => u.UserId == id).ToArray().Select(u => new UserDetailsViewModel()
             {
                 LastName = u.LastName,
                 FirstName = u.FirstName,
@@ -47,6 +48,21 @@ namespace PetsWebsite.Controllers.UserDetailsAPIController
                 Address = u.Address,
                 Birthday = u.Birthday?.ToShortDateString()
             }).FirstOrDefault();
+        }
+
+        [HttpPost]
+        public ActionResult<Boolean> UpdateUserDetails(UserDetailsViewModel uDetails)
+        {
+            int id = 4;
+            var query = _dbContext.Users.FirstOrDefault(u => u.UserId == id);
+            query.LastName = uDetails.LastName;
+            query.FirstName = uDetails.FirstName;
+            query.Email = uDetails.Email;
+            query.Phone = uDetails.Phone;
+            query.Address = uDetails.Address;
+            query.Birthday = Convert.ToDateTime(uDetails.Birthday);
+            _dbContext.SaveChanges();
+            return true;
         }
     }
 }
