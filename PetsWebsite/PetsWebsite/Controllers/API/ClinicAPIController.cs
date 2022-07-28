474c8b541232489c221d9cb7d9fd2a9312760534
Akira <c1410yw123@gmail.com>
2022-07-28 10:43:38 +0800

    Clinicinfo更新

--- PetsWebsite/PetsWebsite/Controllers/API/ClinicAPIController.cs
+++ PetsWebsite/PetsWebsite/Controllers/API/ClinicAPIController.cs
@@ -19,6 +19,7 @@ namespace PetsWebsite.Controllers.API
         [HttpGet]
         public async Task<ActionResult<IEnumerable<Clinic>>> GetAll()
         {
+            var ClinicList = _PetsDB.Clinics.ToList();
             if (_PetsDB.Clinics == null)
             {
                 return NotFound();
@@ -38,6 +39,22 @@ namespace PetsWebsite.Controllers.API
                 return await _PetsDB.Clinics.Where(c => c.City == City&&c.Region == Region).ToListAsync();
             }
         }
-      
+        [HttpGet]
+        [Route("{id}")]
+      public async Task<ActionResult<IEnumerable<Clinic>>> GetInfo([FromRoute(Name ="id")]int Id)
+        {
+            return await _PetsDB.Clinics.Where(i => i.ClinicId == Id).ToListAsync();
+        }
+        [HttpGet]
+        public IEnumerable<Restaurant> GetRestInfo()
+        {
+             var RestList=_PetsDB.Restaurants.ToList();
+            var list = RestList.Take(10);
+            return list;
+        }
+        public IEnumerable<Clinic> GetRestId(int id)
+        {
+            var center = _PetsDB.Clinics.FirstOrDefault(i => i.ClinicId == id);           
+        }
     }
 }
