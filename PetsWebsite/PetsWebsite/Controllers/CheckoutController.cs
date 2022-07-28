8984a53f6fa39126871250df0b4d2baf37ea3e07
Allen <rtallen226@gmail.com>
2022-07-28 16:52:33 +0800

    購物車選擇商品，會員登入後購物車顯示

--- PetsWebsite/PetsWebsite/Controllers/CheckoutController.cs
+++ PetsWebsite/PetsWebsite/Controllers/CheckoutController.cs
@@ -1,25 +1,49 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
+using Newtonsoft.Json;
+using PetsWebsite.Extensions;
 using PetsWebsite.Models;
+using PetsWebsite.Models.ViewModels;
 
 namespace PetsWebsite.Controllers
 {
     public class CheckoutController : Controller
     {
-        private readonly PetsDBContext ProductDBContext;
+        private readonly PetsDBContext _petsDB;
 
-
-        public CheckoutController(PetsDBContext _PetsDBContext)
+        public CheckoutController(PetsDBContext petsDB)
         {
-            ProductDBContext = _PetsDBContext;
+            _petsDB = petsDB;
         }
 
-        //立即購買
-        public IActionResult Index()
+        [Route("Checkout")]
+        public IActionResult CheckOutOrder()
         {
-            
-
             return View();
+        }
+        //立即購買
+        [Route("Checkout")]
+        [HttpPost]
+        public IActionResult CheckOutOrder(int[]? PurProductId)
+        {
+            try
+            {
+                var PurProduct = _petsDB.ShoppingCars.Where(p => PurProductId.Contains(p.ProductId) && p.UserId == User.GetId()).Select(p => new CheckOutOrderViewModle()
+                {
+                    UserId = p.UserId,
+                    ProductId = p.ProductId,
+                    ProductName = p.Product.ProductName,
+                    Count = p.Count,
+                    Price = p.Product.UnitPrice,
+                    PhotoPath = p.Product.PhotoPath
+                }).ToList();
+                HttpContext.Session.SetString("PreOrder", JsonConvert.SerializeObject(PurProduct));
+                return Ok("success");
+            }
+            catch (Exception)
+            {                
+                return BadRequest("something wrong");
+            }
         }
     }
 }
