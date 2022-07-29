b0ff7d81b9da4b4596d3a6eebe20d4eec22bdff1
Akira <c1410yw123@gmail.com>
2022-07-30 04:42:46 +0800

    新增 owl-carousel.min.css owl.carousel.js BackToTop.css BackToTop.js owl-carousel成功

---
+++ PetsWebsite/PetsWebsite/wwwroot/lib/jquery/Powei/BackToTop.js
@@ -0,0 +1,22 @@
+﻿$(document).ready(function () {
+    // 捲軸偵測距離頂部超過 50 才顯示按鈕
+    $(window).scroll(function () {
+        if ($(window).scrollTop() > 50) {
+            if ($(".back-top").hasClass("hide")) {
+                $(".back-top").toggleClass("hide");
+            }
+        } else {
+            $(".back-top").addClass("hide");
+        }
+    });
+
+    // 點擊按鈕回頂部
+    $(".back-top").on("click", function (event) {
+        $("html, body").animate(
+            {
+                scrollTop: 0
+            },
+            100 // 回頂部時間為 100 毫秒
+        );
+    });
+});
