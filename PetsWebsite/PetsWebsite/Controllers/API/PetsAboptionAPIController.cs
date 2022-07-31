5bf9dbd18f3a3416e3a0810cf81df4c1683e1887
LAPTOP-B6B298A8\zu007 <zu00731135@gmail.com>
2022-07-31 21:29:43 +0800

    新增Restaurant頁面分頁, 修改Restaurant頁面樣式, RestaurantDetails頁面加入FaceBook評論, 新增熱門文章容器, 新增領養頁面, 新增領養頁面相關Models

---
+++ PetsWebsite/PetsWebsite/Controllers/API/PetsAboptionAPIController.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using PetsWebsite.Models;
+
+namespace PetsWebsite.Controllers.API
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class PetsAboptionAPIController : ControllerBase
+    {
+        private readonly PetsAboptionResource _petsAboptionResource;
+        public PetsAboptionAPIController(PetsAboptionResource petsAboptionResource)
+        {
+            _petsAboptionResource = petsAboptionResource;
+        }
+
+        [HttpGet]
+        [Route("GetPets")]
+        public ActionResult<IEnumerable<PetsAboption>> GetPets()
+        {
+            HttpClient client = new HttpClient();
+            client.BaseAddress = new Uri(_petsAboptionResource.pets);
+            string jsonString = client.GetStringAsync("").GetAwaiter().GetResult();
+            List<PetsAboption> data = Newtonsoft.Json.JsonConvert.DeserializeObject<List<PetsAboption>>(jsonString);
+           
+            return data;
+
+        }
+    }
+}
---
+++ PetsWebsite/PetsWebsite/Models/PetsAboption.cs
@@ -0,0 +1,36 @@
+﻿namespace PetsWebsite.Models
+{
+    public class PetsAboption
+    {
+        public int animal_id { get; set; }
+        public string animal_subid { get; set; }
+        public int animal_area_pkid { get; set; }
+        public int animal_shelter_pkid { get; set; }
+        public string animal_place { get; set; }
+        public string animal_kind { get; set; }
+        public string animal_Variety { get; set; }
+        public string animal_sex { get; set; }
+        public string animal_bodytype { get; set; }
+        public string animal_colour { get; set; }
+        public string animal_age { get; set; }
+        public string animal_sterilization { get; set; }
+        public string animal_bacterin { get; set; }
+        public string animal_foundplace { get; set; }
+        public string animal_title { get; set; }
+        public string animal_status { get; set; }
+        public string animal_remark { get; set; }
+        public string animal_caption { get; set; }
+        public string animal_opendate { get; set; }
+        public string animal_closeddate { get; set; }
+        public string animal_update { get; set; }
+        public string animal_createtime { get; set; }
+        public string shelter_name { get; set; }
+        public string album_file { get; set; }
+        public string album_update { get; set; }
+        public string cDate { get; set; }
+        public string shelter_address { get; set; }
+        public string shelter_tel { get; set; }
+    }
+
+
+}
