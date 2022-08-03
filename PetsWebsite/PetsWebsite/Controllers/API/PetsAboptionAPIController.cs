e2530356919f224ac9c099ae7cac0b02fef22cb4
AaronChen <zu00731135@gmail.com>
2022-08-03 21:46:25 +0800

    Cache改善, 加入領養篩選渲染功能, 增加關鍵字HighLigh功能

--- PetsWebsite/PetsWebsite/Controllers/API/PetsAboptionAPIController.cs
+++ PetsWebsite/PetsWebsite/Controllers/API/PetsAboptionAPIController.cs
@@ -3,6 +3,7 @@ using Microsoft.AspNetCore.Mvc;
 using PetsWebsite.Models;
 using Newtonsoft.Json;
 using Microsoft.Extensions.Caching.Memory;
+using System.Collections.Generic;
 
 namespace PetsWebsite.Controllers.API
 {
@@ -32,24 +33,39 @@ namespace PetsWebsite.Controllers.API
         }
         [HttpGet]
         [Route("GetPage")]
-        public async Task<IEnumerable<PetsAboption>> GetPage([FromQuery] int curPage, int showPage)
+        public async Task<IEnumerable<PetsAboption>> GetPage([FromQuery] int curPage, int showPage, string? kinds, string? key)
         {
-            client.BaseAddress = new Uri(_petsAboptionResource.pets);
-            string jsonString = await client.GetStringAsync("");
-            List<PetsAboption> data = JsonConvert.DeserializeObject<List<PetsAboption>>(jsonString);
-            var query = data.OrderBy(x => x.animal_id).Skip((curPage - 1) * showPage).Take(showPage).ToList();
-            object CacheData;
-            MemoryCacheEntryOptions memoryCache = new MemoryCacheEntryOptions();
-            memoryCache.SetPriority(CacheItemPriority.Normal);
-            memoryCache.SetAbsoluteExpiration(TimeSpan.FromHours(1));
-            _cache.Set("CacheByQuery", query, memoryCache);
-            if (_cache.TryGetValue("CacheByQuery", out CacheData))
+            if (!_cache.TryGetValue("CacheByQuery", out List<PetsAboption> CacheData))
             {
-                return query = CacheData as List<PetsAboption>;
+                client.BaseAddress = new Uri(_petsAboptionResource.pets);
+                string jsonString = await client.GetStringAsync("");
+                CacheData = JsonConvert.DeserializeObject<List<PetsAboption>>(jsonString);
+                //Cache
+                MemoryCacheEntryOptions memoryCache = new MemoryCacheEntryOptions();
+                memoryCache.SetPriority(CacheItemPriority.Normal);
+                memoryCache.SetAbsoluteExpiration(TimeSpan.FromHours(1));
+                _cache.Set("CacheByQuery", CacheData, memoryCache);
             }
-            return Enumerable.Empty<PetsAboption>();
+            return CacheData.OrderBy(x => x.animal_id)
+                .Where(x => { return kinds == null ? true : x.animal_kind == kinds; })
+                .Where(k => { return key == null ? true : k.animal_Variety.Contains(key) || k.animal_kind.Contains(key) || k.animal_foundplace.Contains(key) || k.animal_place.Contains(key); }).Skip((curPage - 1) * showPage).Take(showPage).ToList();
         }
+        [HttpGet]
+        [Route("GetKeyWord")]
+        public async Task<IEnumerable<PetsAboption>> GetKeyWord([FromQuery]string? key)
+        {
+            if(!_cache.TryGetValue("CacheByKey",out List<PetsAboption> data))
+            {
+                client.BaseAddress = new Uri(_petsAboptionResource.pets);
+                string jsonString = await client.GetStringAsync("");
+                data = JsonConvert.DeserializeObject<List<PetsAboption>>(jsonString);
 
-
+                MemoryCacheEntryOptions cache = new MemoryCacheEntryOptions();
+                cache.SetPriority(CacheItemPriority.Normal);
+                cache.SetAbsoluteExpiration(TimeSpan.FromHours(1));
+                _cache.Set("CacheByKey", data, cache);
+            }
+            return data.Where(k => k.animal_Variety.Contains(key) || k.animal_kind.Contains(key) || k.animal_foundplace.Contains(key) || k.animal_place.Contains(key));
+        }
     }
 }
