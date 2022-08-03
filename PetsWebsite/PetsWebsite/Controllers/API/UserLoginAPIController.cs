2666086ebfed2b817b6141c0ad69873c7393f9e0
AndrewWu <andrewwu0@gmail.com>
2022-08-03 20:45:11 +0800

    新增商品、餐廳上架填寫欄位 / 診所上架審核建議完成

--- PetsWebsite/PetsWebsite/Controllers/API/UserLoginAPIController.cs
+++ PetsWebsite/PetsWebsite/Controllers/API/UserLoginAPIController.cs
@@ -1,6 +1,5 @@
 ﻿using Microsoft.AspNetCore.Authentication;
 using Microsoft.AspNetCore.Authentication.Cookies;
-using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using PetsWebsite.Models;
---
+++ PetsWebsite/PetsWebsite/Models/ViewModels/ProductViewModel.cs
@@ -0,0 +1,20 @@
+﻿namespace PetsWebsite.Models.ViewModels
+{
+    public class ProductViewModel
+    {
+        public int ProductId { get; set; }
+        public string ProductName { get; set; } = null!;
+        public int? CategoryId { get; set; }
+        public decimal? UnitPrice { get; set; }
+        public short? UnitsInStock { get; set; }
+        public string? PhotoPath { get; set; }
+        public string? Describe { get; set; }
+        public DateTime? LaunchDate { get; set; }
+        public DateTime? RemoveDate { get; set; }
+        public int? CompanyId { get; set; }
+        public bool? State { get; set; }
+        public string? Discount { get; set; }
+        public string? Species { get; set; }
+        public string? AuditResult { get; set; }
+    }
+}
