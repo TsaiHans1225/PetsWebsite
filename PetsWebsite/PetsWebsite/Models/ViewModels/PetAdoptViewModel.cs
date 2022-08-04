34e604adb63c53d7e6075bd1b7484bb69af3d4fa
Allen <rtallen226@gmail.com>
2022-08-04 11:19:03 +0800

    修整Bug For 領養搜尋條件,分頁功能

---
+++ PetsWebsite/PetsWebsite/Models/ViewModels/PetAdoptViewModel.cs
@@ -0,0 +1,8 @@
+﻿namespace PetsWebsite.Models.ViewModels
+{
+    public class PetAdoptViewModel
+    {
+        public List<PetsAboption> PetsAdoptions { get; set; }
+        public int Count { get; set; }
+    }
+}
