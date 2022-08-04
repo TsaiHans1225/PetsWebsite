10ceae433fc8f7dd9ba1f124f246641982c8cbac
Allen <rtallen226@gmail.com>
2022-08-04 16:22:19 +0800

    新增餐廳ViewModel

---
+++ PetsWebsite/PetsWebsite/Models/ViewModels/RestArticlesViewModel.cs
@@ -0,0 +1,18 @@
+﻿namespace PetsWebsite.Models.ViewModels
+{
+    public class RestArticlesViewModel
+    {
+        public int RestID { get; set; }
+        public string RestName { get; set; }
+        public string RestPhone { get; set; }
+        public string RestPhotoPath { get; set; }
+        public string RestIntroduction { get; set; }
+        public string RestCity { get; set; }
+        public string RestRegion { get; set; }
+        public string RestAddress { get; set; }
+        public string RestTime { get; set; }
+        public string RestReserve { get; set; }
+        public string Title { get; set; }
+        public string Contents { get; set; }
+    }
+}
