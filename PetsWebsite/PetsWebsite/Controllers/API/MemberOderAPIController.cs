b189b124d15af13e5b2fb81d4c4f44c45ae01802
Allen <rtallen226@gamil.com>
2022-08-06 12:37:29 +0800

    交易完成訂單資訊API

--- PetsWebsite/PetsWebsite/Controllers/API/MemberOderAPIController.cs
+++ PetsWebsite/PetsWebsite/Controllers/API/MemberOderAPIController.cs
@@ -18,8 +18,10 @@ namespace PetsWebsite.Controllers.API
         [HttpGet]
         public MemberOrderViewModel GetMemberOrderInfo()
         {
-            DateTime sample_date = new DateTime(2010, 6, 14);
-            sample_date.ToString();
+            if (HttpContext.Session.GetString("TraderOrderNo") == null)
+            {
+                return null;
+            }
             var OrderInfo = _petsDB.Orders.Where(o => o.OrderId == HttpContext.Session.GetString("TraderOrderNo"))
                 .Select(o=>new MemberOrderViewModel()
                 {
@@ -32,5 +34,55 @@ namespace PetsWebsite.Controllers.API
                 }).FirstOrDefault();
             return OrderInfo;
         }
+        [HttpGet]
+        public List<MemberBuyProductViewModel> GetMemberProductInfo()
+        {
+            if (HttpContext.Session.GetString("TraderOrderNo") == null)
+            {
+                return null;
+            }
+            var PurchaseProductInfo = _petsDB.OrderDetails.Where(o => o.OrderId == HttpContext.Session.GetString("TraderOrderNo"))
+                .Select(o => new MemberBuyProductViewModel()
+                {
+                    ProductName=o.Product.ProductName,
+                    PurchaseCount=o.Counts
+
+                }).ToList();
+            return PurchaseProductInfo;
+        }
+        [HttpGet]
+        public List<OrderHistory> GetMemberOrderHistory()
+        {
+            var query = _petsDB.Orders.Where(o => o.UserId == 4).Select(o => new OrderHistory()
+            {
+                OrderId = o.OrderId,
+                OrderDate = DateTime.Parse(o.OrderDate.ToString()).ToString(),
+                OrderWay = o.PaymentWay,
+                OrderStatus = o.OrderStatusNumber,
+                OrderDetails = o.OrderDetails.ToList().Select(x => new OrderProduct
+                {
+                    ProductName = x.Product.ProductName,
+                    Price = x.Product.UnitPrice,
+                    Count = x.Counts
+                })
+            }).ToList();
+            return query;
+        }
+    }
+    public class OrderHistory
+    {
+
+        public string OrderId { get; set; }
+        public string OrderDate { get; set; }
+        public string OrderWay { get; set; }
+        public int? OrderStatus { get; set; }
+        public IEnumerable<OrderProduct> OrderDetails { get; set; }
+    }
+
+    public class OrderProduct
+    {
+        public string ProductName { get; set; }
+        public decimal? Price { get; set; }
+        public int? Count { get; set; }
     }
 }
